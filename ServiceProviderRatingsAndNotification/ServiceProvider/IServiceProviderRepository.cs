458361cde0e28b4598f013922fd11bfc5f618084
exrezzo <ivan.ardillo@gmail.com>
2023-03-30 19:29:01 +0200

    ServiceProvider folder renamed to Rating

--- ServiceProviderRatingsAndNotification/ServiceProvider/IServiceProviderRepository.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ServiceProviderRatingsAndNotification.ServiceProvider;
-
-public interface IServiceProviderRepository
-{
-    Task<ServiceProvider> GetAsync(Guid id);
-    Task<IEnumerable<int>> GetRatingsAsync(Guid id);
-}
