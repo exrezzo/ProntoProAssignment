16f664a9a72dc0b5a3b69ef63aa49f46cc4a8d17
exrezzo <ivan.ardillo@gmail.com>
2023-04-02 10:50:09 +0200

    mssql db creation and seed with Dockerfile and scripts

---
+++ ServiceProviderRatingsAndNotification/Db/init-db.sql
@@ -0,0 +1,21 @@
+create table ServiceProvider
+(
+    Id   uniqueidentifier default newid() not null
+        primary key,
+    Name nvarchar(50)                     not null
+)
+
+create table ServiceProviderRating
+(
+    ServiceProviderId uniqueidentifier
+        references ServiceProvider,
+    Rating            tinyint
+)
+
+INSERT INTO ServiceProvider (Id, Name) VALUES (N'F4610D85-0EB2-464D-A8F9-04F1FCFBA694', N'Cool SP');
+INSERT INTO ServiceProvider (Id, Name) VALUES (N'C9DA4F99-40E9-484C-BBD5-477C016F9726', N'Apulia Sp SRL');
+
+INSERT INTO ServiceProviderRating (ServiceProviderId, Rating) VALUES (N'C9DA4F99-40E9-484C-BBD5-477C016F9726', 5);
+INSERT INTO ServiceProviderRating (ServiceProviderId, Rating) VALUES (N'C9DA4F99-40E9-484C-BBD5-477C016F9726', 4);
+INSERT INTO ServiceProviderRating (ServiceProviderId, Rating) VALUES (N'F4610D85-0EB2-464D-A8F9-04F1FCFBA694', 3);
+INSERT INTO ServiceProviderRating (ServiceProviderId, Rating) VALUES (N'F4610D85-0EB2-464D-A8F9-04F1FCFBA694', 3);
