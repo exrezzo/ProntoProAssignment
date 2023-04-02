46ae4b20582e059996df4540e6b2f0a81c6c0b5c
exrezzo <ivan.ardillo@gmail.com>
2023-04-02 12:14:29 +0200

    service provider repo implementation with dapper + dependency injection with services

--- ServiceProviderRatingsAndNotification/Program.cs
+++ ServiceProviderRatingsAndNotification/Program.cs
@@ -1,6 +1,17 @@
+using ServiceProviderRatingsAndNotification.Rating;
+using ServiceProviderRatingsAndNotification.ServiceProvider;
+using ServiceProviderRatingsAndNotification.ServiceProviderNotification;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddTransient<IServiceProviderRepository>(_ => 
+// in real word application this should come from appsettings.json, but there the only purpose is to run locally.
+    new ServiceProviderRepository("Server=localhost;Database=spratingsdb;User Id=sa;Password=SuperPass92!")
+);
+builder.Services.AddSingleton<IServiceProviderNotifier>(_ => new ServiceProviderNotifierWithRabbitMq("localhost"));
+builder.Services.AddTransient<RatingService>();
+// builder.Services.AddTransient<ServiceProviderService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- ServiceProviderRatingsAndNotification/Rating/IServiceProviderRepository.cs
+++ ServiceProviderRatingsAndNotification/Rating/IServiceProviderRepository.cs
@@ -1,8 +1,39 @@
-﻿namespace ServiceProviderRatingsAndNotification.Rating;
+﻿using System.Data.SqlClient;
+using Dapper;
+
+namespace ServiceProviderRatingsAndNotification.Rating;
 
 public interface IServiceProviderRepository
 {
     Task<ServiceProvider.ServiceProvider> GetAsync(Guid serviceProviderId);
     Task<IEnumerable<int>> GetRatingsAsync(Guid serviceProviderId);
     Task AddRatingAsync(Guid serviceProviderId, int rating);
+}
+
+public class ServiceProviderRepository : IServiceProviderRepository
+{
+    private readonly string _connectionString;
+
+    public ServiceProviderRepository(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+    public async Task<ServiceProvider.ServiceProvider> GetAsync(Guid serviceProviderId)
+    {
+        await using var sqlConnection = new SqlConnection(_connectionString);
+        var serviceProvider = await sqlConnection.QuerySingleOrDefaultAsync<ServiceProvider.ServiceProvider>("SELECT * FROM ServiceProvider where Id = @serviceProviderId", new {serviceProviderId});
+        return serviceProvider;
+    }
+
+    public async Task<IEnumerable<int>> GetRatingsAsync(Guid serviceProviderId)
+    {
+        await using var sqlConnection = new SqlConnection(_connectionString);
+        var ratings = await sqlConnection.QueryAsync<int>("SELECT Rating FROM ServiceProviderRating where ServiceProviderId = @serviceProviderId", new{serviceProviderId});
+        return ratings;
+    }
+
+    public Task AddRatingAsync(Guid serviceProviderId, int rating)
+    {
+        throw new NotImplementedException();
+    }
 }
