a06482bb5746e121ce7cc53b00037be5215bb90d
Ivan Ardillo <ivan.ardillo@gmail.com>
2023-04-05 17:50:54 +0200

    test coverage increased with RatingTests

--- ServiceProviderRatingsAndNotification.Tests/RatingTests.cs
+++ ServiceProviderRatingsAndNotification.Tests/RatingTests.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using Moq;
+using RabbitMQ.Client;
 using ServiceProviderRatingsAndNotification.Rating;
 using ServiceProviderRatingsAndNotification.ServiceProvider;
 using ServiceProviderRatingsAndNotification.ServiceProviderNotification;
@@ -32,10 +33,33 @@ public class RatingTests
         averageRating.Should().Be(expectedAverage);
     }
 
+    [Fact]
+    public async Task Getting_Average_Rating_For_Non_Existing_Service_Provider_Throws()
+    {
+        var serviceProviderRepoMock = new Mock<IServiceProviderRepository>();
+        var id = Guid.NewGuid();
+        serviceProviderRepoMock
+            .Setup(repository => repository.GetAsync(id))
+            .ReturnsAsync(new ServiceProvider.ServiceProvider()
+            {
+                Id = id,
+                Name = "Cool Service Provider"
+            });
+        serviceProviderRepoMock
+            .Setup(repository =>
+                repository.GetRatingsAsync(id))
+            .ReturnsAsync(new []{1});
+
+        var ratingService = new RatingService(serviceProviderRepoMock.Object, Mock.Of<IServiceProviderNotifier>());
+        var averageRatingFunc = async () => await ratingService.GetAverageRatingForServiceProvider(Guid.NewGuid());
+
+        await averageRatingFunc.Should().ThrowExactlyAsync<ServiceProviderNotFoundException>();
+    }
+
     [Theory]
     [InlineData(0)]
     [InlineData(6)]
-    public async Task Adding_Invalid_Rating_Throws(int invalidRating)
+    public async Task Adding_Invalid_Rating_Throws(uint invalidRating)
     {
         var serviceProviderRepoMock = new Mock<IServiceProviderRepository>();
         var id = Guid.NewGuid();
@@ -55,5 +79,69 @@ public class RatingTests
         await throwingSubmission.Should().ThrowExactlyAsync<ArgumentOutOfRangeException>();
     }
 
+    [Fact]
+    public void Rating_Submission_Works_Correctly()
+    {
+        var serviceProviderRepoMock = new Mock<IServiceProviderRepository>();
+        var id = Guid.NewGuid();
+        serviceProviderRepoMock
+            .Setup(repository => repository.GetAsync(id))
+            .ReturnsAsync(new ServiceProvider.ServiceProvider()
+            {
+                Id = id,
+                Name = "Cool Service Provider"
+            });
+        var ratingService = new RatingService(serviceProviderRepoMock.Object, Mock.Of<IServiceProviderNotifier>());
+        var possibleRatings = Enumerable.Range(1, 5).Select(i => (uint) i);
 
+       possibleRatings
+            .Select<uint, Func<Task>>(rating => 
+                async () => await ratingService.SubmitRating(rating, id))
+            .Should()
+            .AllSatisfy(func => 
+                func.Should().NotThrowAsync("Acceptable ratings are in the range 1 to 5"));
+    }
+
+    [Fact]
+    public void Getting_Last_Submitted_Ratings()
+    {
+        var serviceProviderRepoMock = new Mock<IServiceProviderRepository>();
+        var serviceProviderNotifierMock = new Mock<IServiceProviderNotifier>();
+
+        serviceProviderNotifierMock
+            .Setup(notifier =>
+                notifier.GetLastRatingSubmissions(It.IsAny<uint>()))
+            .Returns(new List<string>() { "00000000-0000-0000-0000-000000000000 1" });
+
+        var ratingService = new RatingService(serviceProviderRepoMock.Object, serviceProviderNotifierMock.Object);
+
+
+        var lastRatingSubmissions = ratingService.GetLastRatingSubmissions(1);
+        lastRatingSubmissions
+            .First()
+            .Should()
+            .BeEquivalentTo(new RatingSubmission()
+            {
+                ServiceProviderId = Guid.Parse("00000000-0000-0000-0000-000000000000"),
+                Rating = 1
+            });
+    }
+
+    [Fact]
+    public void Getting_A_Malformed_Rating_Submission_Message_Should_Throw()
+    {
+        var serviceProviderRepoMock = new Mock<IServiceProviderRepository>();
+        var serviceProviderNotifierMock = new Mock<IServiceProviderNotifier>();
+
+        serviceProviderNotifierMock
+            .Setup(notifier =>
+                notifier.GetLastRatingSubmissions(It.IsAny<uint>()))
+            .Returns(new List<string>() { "00000000-invalid-Guid 1992" });
+
+        var ratingService = new RatingService(serviceProviderRepoMock.Object, serviceProviderNotifierMock.Object);
+
+
+        var submissionsGettingFunc = () => ratingService.GetLastRatingSubmissions(1).ToList();
+        submissionsGettingFunc.Should().ThrowExactly<InvalidRatingSubmissionMessageException>();
+    }
 }
