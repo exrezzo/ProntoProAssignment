9c582e3558311cb7347eefa38cb7bb9893c33af3
Ivan Ardillo <ivan.ardillo@gmail.com>
2023-04-07 11:23:08 +0200

    Repo and Notification fixtures created for integration testing

---
+++ ServiceProviderRatingsAndNotification.Tests/Integration/Fixture/NotifierFixture.cs
@@ -0,0 +1,34 @@
+﻿using ServiceProviderRatingsAndNotification.ServiceProviderNotification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testcontainers.RabbitMq;
+
+namespace ServiceProviderRatingsAndNotification.Tests.Integration.Fixture
+{
+    public class NotifierFixture : IAsyncDisposable
+    {
+        private readonly RabbitMqContainer _rabbitMqContainer;
+        public IServiceProviderNotifier ServiceProviderNotifier { get; private set; }
+
+        public NotifierFixture()
+        {
+            _rabbitMqContainer = new RabbitMqBuilder()
+                .WithUsername("guest")
+                .WithPassword("guest")
+                .Build();
+
+            _rabbitMqContainer.StartAsync().Wait();
+            ServiceProviderNotifier = new ServiceProviderNotifierWithRabbitMq(
+                _rabbitMqContainer.Hostname,
+                _rabbitMqContainer.GetMappedPublicPort(5672));
+        }
+        public async ValueTask DisposeAsync()
+        {
+            await _rabbitMqContainer.StopAsync();
+            await _rabbitMqContainer.DisposeAsync();
+        }
+    }
+}
---
+++ ServiceProviderRatingsAndNotification.Tests/Integration/Fixture/RepositoriesFixture.cs
@@ -0,0 +1,38 @@
+﻿using ServiceProviderRatingsAndNotification.ServiceProvider;
+using ServiceProviderRatingsAndNotification.ServiceProviderNotification;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testcontainers.MsSql;
+using Testcontainers.RabbitMq;
+
+namespace ServiceProviderRatingsAndNotification.Tests.Integration.Fixture
+{
+    public class RepositoriesFixture : IAsyncDisposable
+    {
+        private readonly MsSqlContainer _container;
+
+        public IServiceProviderRepository ServiceProviderRepository { get; }
+
+        public RepositoriesFixture()
+        {
+            _container = new MsSqlBuilder().Build();
+            _container.StartAsync().Wait();
+            var connectionString = _container.GetConnectionString();
+            var execScriptAsync = _container.ExecScriptAsync(File.ReadAllText("Integration/Fixture/init-db.sql")).Result;
+
+            ServiceProviderRepository = new ServiceProviderRepository(connectionString);
+
+            
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _container.StopAsync();
+            await _container.DisposeAsync();
+        }
+    }
+}
